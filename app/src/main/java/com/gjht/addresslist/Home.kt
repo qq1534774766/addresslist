f7cf94b50e9eeee21d23f2769cf691696dfc4bb9
aguo <aguowqg@qq.com>
2022-06-08 21:30:09 +0800

    完成数据增删改查操作

---
+++ app/src/main/java/com/gjht/addresslist/Home.kt
@@ -0,0 +1,13 @@
+package com.gjht.addresslist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+
+}
---
+++ app/src/main/java/com/gjht/addresslist/User.kt
@@ -0,0 +1,3 @@
+package com.gjht.addresslist
+
+data class User(val name:String, val phone:String,val url:String)
